ca7ee52b9449385cc23949a0a786df409beb57ad
filippahans <hanssonfilippa@hotmail.com>
2024-04-10 14:35:51 +0200

    Update Camel

---
+++ camel.js
@@ -0,0 +1,19 @@
+let camelX = 100;
+let camelY = 100;
+
+//start position camel
+
+//camel head
+fill(219, 165, 119);
+ellipse(camelX + 155, camelY + 150, camelX - 10, camelY - 10);
+
+//camel nose
+noStroke();
+fill(219, 165, 119);
+ellipse(camelX + 179, camelY + 160, camelX - 25, camelY - 25);
+
+//camel ears
+fill(219, 165, 119);
+ellipse(camelX + 155, camelY + 110, camelX - 90, camelY - 30);
+
+ellipse(camelX + 128, camelY + 120, camelX - 90, camelY - 30);
