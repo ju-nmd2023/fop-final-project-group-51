50a237ceadf1d2a844e8333c3ea2017bf5de3404
filippahans <hanssonfilippa@hotmail.com>
2024-04-11 15:06:03 +0200

    update camel

--- camel.js
+++ camel.js
@@ -17,3 +17,26 @@ fill(219, 165, 119);
 ellipse(camelX + 155, camelY + 110, camelX - 90, camelY - 30);
 
 ellipse(camelX + 128, camelY + 120, camelX - 90, camelY - 30);
+
+//camel throat
+ellipse(camelX + 123, camelY + 200, camelX - 60, camelY - 250);
+
+//camel body
+ellipse(camelX + 35, camelY + 255, camelX - 300, camelY - 0);
+
+//camel legs
+ellipse(camelX + 100, camelY + 320, camelX - 90, camelY - 0);
+ellipse(camelX + 80, camelY + 320, camelX - 90, camelY - 0);
+
+ellipse(camelX - 10, camelY + 320, camelX - 90, camelY - 0);
+ellipse(camelX - 30, camelY + 320, camelX - 90, camelY - 0);
+
+//camel tail
+ellipse(camelX - 65, camelY + 255, camelX - 50, camelY - 110);
+
+//camel blanket
+fill(211, 117, 100);
+ellipse(camelX + 30, camelY + 210, camelX - 50, camelY + 10);
+
+ellipse(camelX + 0, camelY + 220, camelX - 50, camelY - 10);
+ellipse(camelX + 55, camelY + 220, camelX - 50, camelY - 10);
