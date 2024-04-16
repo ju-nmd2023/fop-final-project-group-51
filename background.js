dfcdb54af61fea95a9c6bca5bb188c5750145680
pafr23wt <148805767+pafr23wt@users.noreply.github.com>
2024-04-16 18:05:16 +0200

    mud

--- background.js
+++ background.js
@@ -17,3 +17,25 @@ ellipse(x + 350, y + 400, 250, 300);
 fill(202, 143, 66);
 noStroke();
 rect(x - 200, y + 400, 600, 300);
+
+//mud
+fill(101, 70, 33);
+noStroke();
+ellipse(x - 100, y + 640, 300);
+ellipse(x + 200, y + 620, 500, 300);
+fill(88, 57, 39);
+noStroke();
+ellipse(x - 100, y + 520, 20);
+ellipse(x - 140, y + 560, 20);
+ellipse(x - 80, y + 570, 30, 20);
+ellipse(x, y + 570, 40, 30);
+ellipse(x, y + 570, 30);
+ellipse(x + 100, y + 570, 30);
+ellipse(x + 70, y + 520, 20);
+ellipse(x + 140, y + 520, 20);
+ellipse(x + 200, y + 550, 30);
+ellipse(x + 300, y + 550, 30);
+ellipse(x + 250, y + 520, 20, 15);
+ellipse(x + 350, y + 570, 35);
+
+//cactus
