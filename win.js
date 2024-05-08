d1964e39365cba569775d4ee754d38fa39679ac1
pafr23wt <148805767+pafr23wt@users.noreply.github.com>
2024-05-08 14:15:08 +0200

    upd winScreen

--- win.js
+++ win.js
@@ -8,6 +8,58 @@ let cloudY = 200;
 let sunX = 200;
 let sunY = 200;
 
+let waterTankX = 100;
+let waterTankY = 100;
+
+let waterDropX = 100;
+let waterDropY = 100;
+
+//watertank
+function waterTank(waterTankX, waterTankY) {
+  noStroke();
+  fill(121, 205, 244);
+  ellipse(waterTankX + 100, waterTankY + 55, 70, 80);
+
+  ellipse(waterTankX + 100, waterTankY + 80, 70, 20);
+  ellipse(waterTankX + 100, waterTankY + 90, 70, 20);
+  ellipse(waterTankX + 100, waterTankY + 100, 70, 20);
+  ellipse(waterTankX + 100, waterTankY + 110, 70, 20);
+  ellipse(waterTankX + 100, waterTankY + 120, 70, 20);
+  ellipse(waterTankX + 100, waterTankY + 130, 70, 20);
+  ellipse(waterTankX + 100, waterTankY + 140, 70, 20);
+  rect(waterTankX + 65, waterTankY + 142, 70, 20, 20);
+
+  //lid
+  fill(187, 189, 191);
+  rect(waterTankX + 85, waterTankY, 30, 20);
+
+  //lid stripes
+  fill(0, 0, 0);
+  rect(waterTankX + 88, waterTankY, 1, 20);
+  rect(waterTankX + 91, waterTankY, 1, 20);
+  rect(waterTankX + 94, waterTankY, 1, 20);
+  rect(waterTankX + 97, waterTankY, 1, 20);
+
+  //white reflection
+  fill(255, 255, 255);
+  ellipse(waterTankX + 95, waterTankY + 55, 50, 60);
+  fill(121, 205, 244);
+  ellipse(waterTankX + 98, waterTankY + 55, 50, 70);
+}
+
+function waterDrop(waterDropX, waterDropY) {
+  //waterdrop
+  noStroke();
+  fill(121, 205, 244);
+  ellipse(waterDropX + 250, waterDropY + 55, 20, 40);
+
+  //reflection
+  fill(255, 255, 255);
+  ellipse(waterDropX + 248, waterDropY + 55, 10, 30);
+  fill(121, 205, 244);
+  ellipse(waterDropX + 250, waterDropY + 55, 10, 30);
+}
+
 function dunes(x, y) {
   //sky
   background(239, 227, 210);
@@ -218,12 +270,13 @@ for (let i = 0; i < 1000; i++) {
 
 function winScreen() {
   dunes(x, y);
+  camel(camelX, camelY, scale);
   sun();
   textSize(50);
   fill(53, 180, 40);
   text("YOU WON", 150, 300);
   textSize(30);
-  fill(255);
+  fill(53, 180, 40);
   text("Click to restart", 180, 340);
   for (let star of stars) {
     const randomValue = Math.random();
@@ -249,4 +302,7 @@ function winScreen() {
 
 function draw() {
   winScreen();
+  camel(camelX + 90, camelY + 200, 1); //scale down the camel
+  waterTank(waterTankX + 190, waterTankY + 400);
+  waterDrop(waterDropX - 15, waterDropY + 270);
 }
